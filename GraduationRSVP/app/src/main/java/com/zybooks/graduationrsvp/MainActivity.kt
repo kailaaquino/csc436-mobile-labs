468d3c2ccb76affd5d023a49d1261d61cca4ba9e
Kaila Aquino <kailad.aquino@gmail.com>
2025-09-26 09:09:38 -0700

    add resource string

--- GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
+++ GraduationRSVP/app/src/main/java/com/zybooks/graduationrsvp/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -36,19 +37,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun GradScreen(modifier: Modifier = Modifier) {
-    Image(
-        painter = painterResource(R.drawable.grad_cap),
-        contentDescription = "Graduation cap",
-        alpha = 0.3f
-    )
     Text(
-        text = "Graduation Announcement",
+        text = stringResource(R.string.graduation_announcement),
         fontSize = 50.sp,
         modifier = modifier,
         lineHeight = 50.sp,
         fontWeight = FontWeight.Bold,
         color = Color.Red,
         textAlign = TextAlign.Center
+    )
+    Image(
+        painter = painterResource(R.drawable.grad_cap),
+        contentDescription = "Graduation cap",
+        alpha = 0.3f
     )
 }
 
